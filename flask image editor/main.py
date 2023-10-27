62d349201848ea9b67b3b82a0b987214336beb89
Varun <varunr.nair@outlook.com>
2023-10-28 01:09:10 +0530

    added new file

--- flask image editor/main.py
+++ flask image editor/main.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+app.run(debug=True)
