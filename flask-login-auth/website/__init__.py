8a98c1ebd44aefdb5b604f33031f74bf35d358f1
Varun <varunr.nair@outlook.com>
2023-10-29 19:07:09 +0530

    added new code

--- flask-login-auth/website/__init__.py
+++ flask-login-auth/website/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
@@ -26,3 +32,9 @@ def create_app():
         return User.query.get(int(id))
 
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
