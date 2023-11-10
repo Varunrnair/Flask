bd397dc6c258845c395adcb08980ddb8551b2f72
Varun <varunr.nair@outlook.com>
2023-11-11 00:26:11 +0530

    fixed errors

--- flask-login-auth/website/admin/__init__.py
+++ flask-login-auth/website/admin/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from website.models import User, Note
+from website.models import *
 from website.db import db
 
 #admin_bp = Blueprint('admin', __name__)
@@ -15,6 +15,6 @@ def admin_home():
 """
 
 admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Note, db.session))
+admin.add_view(PostView(Note, db.session))
  #   return render_template('admin/admin_home.html', user=current_user)
 
